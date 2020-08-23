c99a05ec6a9752889d72b958a1575420ceee8d41
bdubeJHB <bdube@student.wethinkcode.co.za>
2020-08-23 16:55:28 +0200

    wtc-lms save

--- hangman.py
+++ hangman.py
@@ -7,11 +7,11 @@ def read_file(file_name):
 
 
 def get_user_input():
-    return input('Guess the missing letter: ')
+        return input('Guess the missing letter: ')
 
 
 def ask_file_name():
-    file_name = input("Words file? [leave empty to use short_words.txt] : ")
+    file_name = input("Words file? [leave empty to use short_words.txt] : ").strip()
     if not file_name:
         return 'short_words.txt'
     return file_name
@@ -66,27 +66,46 @@ def do_correct_answer(original_word, answer, guess):
 
 # TODO: Step 4: update to use number of remaining guesses
 def do_wrong_answer(answer, number_guesses):
-    print('Wrong! Number of guesses left: '+str(number_guesses))
+    if number_guesses:
+        print('Wrong! Number of guesses left: ' + str(number_guesses))
+    else:
+        print("Sorry, you are out of guesses. The word was " + answer)
     draw_figure(number_guesses)
 
 
 # TODO: Step 5: draw hangman stick figure, based on number of guesses remaining
 def draw_figure(number_guesses):
-    print('TODO')
+    if number_guesses == 5:
+        print("/----\n|\n|\n|\n|")
+    elif number_guesses == 4:
+        print("/----\n|   0\n|\n|\n|\n")
+    elif number_guesses == 3:
+        print("/----\n|   0\n|  /|\\\n|\n|")
+    elif number_guesses == 2:
+        print("/----\n|   0\n|  /|\\\n|   |\n|")
+    else:
+        print("/----\n|   0\n|  /|\\\n|   |\n|  / \\")
 
 # TODO: Step 2 - update to loop over getting input and checking until whole word guessed
 # TODO: Step 3 - update loop to exit game if user types `exit` or `quit`
 # TODO: Step 4 - keep track of number of remaining guesses
 def run_game_loop(word, answer):
-    while 1:
+    #print(answer + " && " + word)
+    lives = 5
+
+    while lives:
+        print("Guess the word: " + answer)
         if '_' not in answer:
             break
-        print("Guess the word: " + answer)
-        guess = get_user_input()
+        guess = get_user_input().strip().lower()
         if is_missing_char(word, answer, guess):
             answer = do_correct_answer(word, answer, guess)
+        elif guess == "quit" or guess == "exit":
+            print("Bye!")
+            break
         else:
-            do_wrong_answer(answer, 0)
+            do_wrong_answer(answer, lives)
+            lives -= 1
 
 
 # TODO: Step 6 - update to get words_file to use from commandline argument
